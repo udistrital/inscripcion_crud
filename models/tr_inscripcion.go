153b36f973cd577bd96554b46dfe1979e0966238
JuanPiedrahita <juangonzalez1597@gmail.com>
2019-12-01 17:27:15 -0500

    udistrital/sga_cliente#97 transacción para registrar datos de transferencia interna

--- models/tr_inscripcion.go
+++ models/tr_inscripcion.go
@@ -11,6 +11,10 @@ type TrInscripcionReintegro struct {
 	ReintegroEstudiante   *Reintegro
 }
 
+type TrInscripcionTransferencia struct {
+	InscripcionEstudiante *Inscripcion
+	TransferenciaEstudiante   *Transferencia
+}
 
 // AddTrInscripcionReintegro Transacción para registrar toda la información de un reintegro
 func AddTrInscripcionReintegro(m *TrInscripcionReintegro) (err error) {
@@ -29,6 +33,35 @@ func AddTrInscripcionReintegro(m *TrInscripcionReintegro) (err error) {
 
 	if idReintegro, errTr := o.Insert(m.ReintegroEstudiante); errTr == nil {
 		fmt.Println("Reintegro registrado, Id: ", idReintegro)
+	} else {
+		err = errTr
+		fmt.Println(err)
+		_ = o.Rollback()
+		return
+	}
+
+	_ = o.Commit()
+
+	return
+}
+
+// AddTrInscripcionTransferencia Transacción para registrar toda la información de una transferencia
+func AddTrInscripcionTransferencia(m *TrInscripcionTransferencia) (err error) {
+	o := orm.NewOrm()
+	err = o.Begin()
+	
+	if idInscripcion, errTr := o.Insert(m.InscripcionEstudiante); errTr == nil {
+		fmt.Println("Inscripcion registrada, Id: ", idInscripcion)
+		m.TransferenciaEstudiante.InscripcionId.Id = int(idInscripcion)
+	} else {
+		err = errTr
+		fmt.Println(err)
+		_ = o.Rollback()
+		return
+	}
+
+	if idTransferencia, errTr := o.Insert(m.TransferenciaEstudiante); errTr == nil {
+		fmt.Println("Transferencia registrada, Id: ", idTransferencia)
 	} else {
 		err = errTr
 		fmt.Println(err)
