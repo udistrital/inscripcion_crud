9a2496c4b3b64aa11a31924a5cedb25806ae10d2
Fabian Barreto Sanchez <macfabian@MacBook-Air-de-Fabian.local>
2020-02-10 14:27:39 -0500

    Se migrations para los inserte de los distintos estados de admision necesarios para la tarea

---
+++ database/scripts/20200210_135821_insert_estados_inscripcion_admision.up.sql
@@ -0,0 +1,6 @@
+insert into inscripcion.estado_inscripcion (nombre, descripcion, codigo_abreviacion, activo, numero_orden, fecha_creacion, fecha_modificacion) 
+	values ('ADMITIDO', 'El aspirante es admitido', 'ADM', true, 2, now(), now());
+insert into inscripcion.estado_inscripcion (nombre, descripcion, codigo_abreviacion, activo, numero_orden, fecha_creacion, fecha_modificacion) 
+	values ('OPCIONADO', 'El aspirante es opcionado', 'OPC', true, 3, now(), now());
+insert into inscripcion.estado_inscripcion (nombre, descripcion, codigo_abreviacion, activo, numero_orden, fecha_creacion, fecha_modificacion) 
+	values ('NO ADMITIDO', 'El aspirante es no admitido', 'NOADM', true, 3, now(), now());
