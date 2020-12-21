7fa14b4c3a5f3dc5b74862ebc3d89483aca50ee4
Ricardo Avendaño Casas <ricardoavendanocasas@MacBook-Pro-de-Ricardo.local>
2020-12-21 13:04:21 -0500

    fix: agregar campo especial en tabla tipo_inscripcion

--- models/tipo_inscripcion.go
+++ models/tipo_inscripcion.go
@@ -10,15 +10,16 @@ import (
 )
 
 type TipoInscripcion struct {
-	Id                int       `orm:"column(id);pk;auto"`
-	Nombre            string    `orm:"column(nombre)"`
-	Descripcion       string    `orm:"column(descripcion);null"`
-	CodigoAbreviacion string    `orm:"column(codigo_abreviacion)"`
-	Activo            bool      `orm:"column(activo)"`
-	NumeroOrden       float64   `orm:"column(numero_orden);null"`
-	NivelId           int       `orm:"column(nivel_id)"`
-	FechaCreacion     string    `orm:"column(fecha_creacion);type(timestamp without time zone)"`
-	FechaModificacion string    `orm:"column(fecha_modificacion);type(timestamp without time zone)"`
+	Id                int     `orm:"column(id);pk;auto"`
+	Nombre            string  `orm:"column(nombre)"`
+	Descripcion       string  `orm:"column(descripcion);null"`
+	CodigoAbreviacion string  `orm:"column(codigo_abreviacion)"`
+	Activo            bool    `orm:"column(activo)"`
+	NumeroOrden       float64 `orm:"column(numero_orden);null"`
+	NivelId           int     `orm:"column(nivel_id)"`
+	FechaCreacion     string  `orm:"column(fecha_creacion);type(timestamp without time zone)"`
+	FechaModificacion string  `orm:"column(fecha_modificacion);type(timestamp without time zone)"`
+	Especial          bool    `orm:"column(especial)"`
 }
 
 func (t *TipoInscripcion) TableName() string {
