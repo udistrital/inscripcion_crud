c5a07c1526180f078d5c73ce2c5540d66b6867e9
Juanpa <jupmorenor@gmail.com>
2021-01-17 19:51:01 -0500

    fix: Se agrega funcion relatedSel en controladores nuevos de acuerdo a lineamiento. udistrital/sga_cliente#390

--- models/tipo_documento_programa.go
+++ models/tipo_documento_programa.go
@@ -52,7 +52,7 @@ func GetTipoDocumentoProgramaById(id int) (v *TipoDocumentoPrograma, err error) 
 func GetAllTipoDocumentoPrograma(query map[string]string, fields []string, sortby []string, order []string,
 	offset int64, limit int64) (ml []interface{}, err error) {
 	o := orm.NewOrm()
-	qs := o.QueryTable(new(TipoDocumentoPrograma))
+	qs := o.QueryTable(new(TipoDocumentoPrograma)).RelatedSel()
 	// query k=v
 	for k, v := range query {
 		// rewrite dot-notation to Object__Attribute
