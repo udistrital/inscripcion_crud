1cd14809cf0ba7c78801a7f9794cc3c822bfe4e8
gabrielablanco <ferblanco1003@gmail.com>
2021-01-20 16:24:05 -0500

    fix: ajuste del nombre del campo recibo_inscripcion_id a recibo_inscripcion y su tipo de dato de integer a varchar

---
+++ database/scripts/20210120_152128_cambiar_tipo_dato_recibo_inscripcion.up.sql
@@ -0,0 +1,4 @@
+ALTER TABLE inscripcion.inscripcion ALTER COLUMN recibo_inscripcion_id SET DATA TYPE varchar;
+ALTER TABLE inscripcion.inscripcion RENAME COLUMN recibo_inscripcion_id TO recibo_inscripcion;
+
+
