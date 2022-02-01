a2bb927c31813bf074ee8046e8a36979538dd6aa
JQuinteroC <jolquinteroc@correo.udistrital.edu.co>
2022-02-01 02:29:02 -0500

    feat: ajustes en el modelo de reintegro y transferencia. udistrital/sga_cliente#857

---
+++ database/scripts/20220201_004042_agregar_campos_transferencia_reintegro.down.sql
@@ -0,0 +1,5 @@
+ALTER TABLE inscripcion.transferencia DROP COLUMN nota_final;
+ALTER TABLE inscripcion.transferencia DROP COLUMN documento_id;
+
+ALTER TABLE inscripcion.reintegro DROP COLUMN nota_final;
+ALTER TABLE inscripcion.reintegro DROP COLUMN documento_id;
