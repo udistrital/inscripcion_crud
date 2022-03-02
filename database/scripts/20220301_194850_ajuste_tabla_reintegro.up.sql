7ca652f031a7d58172bd3105eb0e4b96b545456b
JQuinteroC <jolquinteroc@correo.udistrital.edu.co>
2022-03-01 20:09:34 -0500

    feat: ajuste en tabla de reingreso. udistrital/sga_cliente#860

---
+++ database/scripts/20220301_194850_ajuste_tabla_reintegro.up.sql
@@ -0,0 +1,3 @@
+ALTER TABLE inscripcion.reintegro ADD COLUMN ultimo_semestre_cursado NUMERIC(5,0) NOT NULL;
+-- ddl-end --
+COMMENT ON COLUMN inscripcion.reintegro.ultimo_semestre_cursado IS 'Último semestre cursado por el estudiante de la carreara de la que proviene.';
