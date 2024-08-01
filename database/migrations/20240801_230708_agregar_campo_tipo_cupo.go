01131751be49595b32a4505ef87a8032590016ee
JaredPeralta <90805689+JaredPeralta@users.noreply.github.com>
2024-08-01 23:16:27 +1000

    feat: udistrital/sga_documentacion#381 Se anade el campo requerido para poder vincular una inscripcion a un tipo de cupo

---
+++ database/migrations/20240801_230708_agregar_campo_tipo_cupo.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"github.com/astaxie/beego/migration"
+)
+
+// DO NOT MODIFY
+type AgregarCampoTipoCupo_20240801_230708 struct {
+	migration.Migration
+}
+
+// DO NOT MODIFY
+func init() {
+	m := &AgregarCampoTipoCupo_20240801_230708{}
+	m.Created = "20240801_230708"
+
+	migration.Register("AgregarCampoTipoCupo_20240801_230708", m)
+}
+
+// Run the migrations
+func (m *AgregarCampoTipoCupo_20240801_230708) Up() {
+	// use m.SQL("CREATE TABLE ...") to make schema update
+	m.SQL("ALTER TABLE IF EXISTS inscripcion.inscripcion ADD COLUMN tipo_cupo integer;")
+
+}
+
+// Reverse the migrations
+func (m *AgregarCampoTipoCupo_20240801_230708) Down() {
+	// use m.SQL("DROP TABLE ...") to reverse schema update
+	m.SQL("ALTER TABLE IF EXISTS inscripcion.inscripcion DROP COLUMN tipo_cupo;")
+
+}
